a10c75e046a021c0853deeb8a229e6adf6f90845
Matheus Duarte <matheus.sduarte.ms@gmail.com>
2026-09-09 23:54:02 -0300

    Obtendo um usuario por id

--- src/main/java/com/matheusduarte/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusduarte/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.matheusduarte.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheusduarte.workshopmongo.domain.User;
 import com.matheusduarte.workshopmongo.repository.UserRepository;
+import com.matheusduarte.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> fingAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
