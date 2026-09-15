cf9377ff9df203ebcd76abddfbdda01be1800822
Matheus Duarte <matheus.sduarte.ms@gmail.com>
2026-09-14 23:58:03 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/matheusduarte/workshopmongo/domain/Post.java
+++ src/main/java/com/matheusduarte/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.matheusduarte.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.matheusduarte.workshopmongo.dto.AuthorDTO;
+import com.matheusduarte.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -21,6 +24,8 @@ public class Post implements Serializable {
 	private String body;
 	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -74,6 +79,16 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -90,5 +105,4 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-
 }
